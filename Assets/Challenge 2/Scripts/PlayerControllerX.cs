7076dc53ebfaacd50f92e802d0e430cae92e2d2f
IMeeus <maeleeuxs@gmail.com>
2026-03-27 22:11:31 +0100

    Finish challenge

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    private readonly float _inputDelay = 1;
+
+    private float _lastActionTime = 0;
+
     public GameObject dogPrefab;
     public InputActionReference sendDogAction;
 
@@ -12,7 +14,11 @@ public class PlayerControllerX : MonoBehaviour
     {
         if (sendDogAction.action.WasPressedThisFrame())
         {
+            if (Time.time - _lastActionTime < _inputDelay)
+                return;
+
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-        }   
+            _lastActionTime = Time.time;
+        }
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
@@ -10,19 +9,30 @@ public class SpawnManagerX : MonoBehaviour
     private readonly float _spawnLimitXRight = 7;
     private readonly float _spawnPosY = 30;
     private readonly float _startDelay = 1.0f;
-    private readonly float _spawnInterval = 4.0f;
+    private readonly float _minSpawnInterval = 3;
+    private readonly float _maxSpawnInterval = 5;
 
-    // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating(nameof(SpawnRandomBall), _startDelay, _spawnInterval);
+        StartCoroutine(nameof(SpawnRandomBallRoutine));
     }
 
-    void SpawnRandomBall ()
+    IEnumerator SpawnRandomBallRoutine()
+    {
+        yield return new WaitForSeconds(_startDelay);
+
+        while (true)
+        {
+            SpawnRandomBall();
+            float randomInterval = Random.Range(_minSpawnInterval, _maxSpawnInterval);
+            yield return new WaitForSeconds(randomInterval);
+        }
+    }
+
+    void SpawnRandomBall()
     {
         var spawnPos = new Vector3(Random.Range(_spawnLimitXLeft, _spawnLimitXRight), _spawnPosY, 0);
         var ballIndex = Random.Range(0, ballPrefabs.Length);
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
-
 }
